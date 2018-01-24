8faa322744351b4e4df0ad15b13cd5a41e4ba946
Martin Michael Zier <martin.michael.zier@artcom.de>
2018-01-24 17:53:32 +0100

    initial commit

---
+++ Assets/Script/CirclePlacement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(CirclePlacement))]
+[CanEditMultipleObjects]
+public class CirclePlacementEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		var tgt = target as CirclePlacement;
+		if(tgt == null) {
+			return;
+		}
+		if(GUILayout.Button("Recalc")) {
+			tgt.PlacePositions();
+		}
+	}
+}
+#endif
+
+public class CirclePlacement : MonoBehaviour {
+
+	public enum FaceDirection {
+		East  = 0,
+		North = 1,
+		West  = 2,
+		South = 3
+	}
+	public FaceDirection facing;
+
+	[Range(0, 25)]
+	public float distance;
+	[Range(-180, 180)]
+	public float rho;
+
+	float _rho {
+		get {
+			return Mathf.Deg2Rad * (rho + (int) facing * 90);
+		}
+	}
+
+	void Start () {
+		PlacePositions();
+	}
+
+	public void PlacePositions() {
+		transform.localPosition = new Vector3(Mathf.Cos(_rho) * distance, transform.localPosition.y, Mathf.Sin(_rho) * distance);
+	}
+}
---
+++ Assets/Script/Editor/SingleModControllerEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(SingleMobController))]
+public class SingleModControllerEditor : Editor {
+
+	public void OnSceneGUI() {
+		var tgt = target as SingleMobController;
+		if(!tgt.debug) {
+			return;
+		}
+		if(Application.isPlaying) {
+			Handles.color = Color.green;
+			Handles.DrawWireDisc(tgt.originalPosition, Vector3.up, tgt.roamingRadius);
+			Handles.color = Color.red;
+			Handles.DrawWireDisc(tgt.transform.position, Vector3.up, tgt.aggressiveRadius);
+			return;
+		}
+		Handles.color = Color.green;
+		Handles.DrawWireDisc(tgt.transform.position, Vector3.up, tgt.roamingRadius);
+		Handles.color = Color.red;
+		Handles.DrawWireDisc(tgt.transform.position, Vector3.up, tgt.aggressiveRadius);
+	}
+}
